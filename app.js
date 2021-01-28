407aee91f02669f896c60da5032bfac2854dd6a7
MOHAMMAD ALMOMANI <MKHM.WORK@GMAIL.COM>
2021-01-28 17:22:02 +0200

    add some ordar option and show the user what thay like to eat in image slide show

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
+var flag = true;
+while(flag  ==true){
+    var Name1 =prompt('Whats your Name ').toUpperCase();
+    ;
+    if(Name1){
+        alert('Wlcome :- '+ Name1 )
+        flag=false ;
+    }
+    else {
+        alert('You should type your Name' );
+    }
+}
 
-var bill =0;
+
+
+var flag2 = true;
+var Order ="";
+while(!(Order =="BURGER"|| Order =="PIZZA")){
+    Order =prompt('WHAT\'S YOUR FAVORITE FOOD, BURGER OR PIZZA ?').toUpperCase();
+
+    
+    if(Order =="BURGER"){
+        document.getElementById("burger").style.display = "block";
+        
+    }
+    else{
+        document.getElementById("PIZZA").style.display = "block";
+    }
+}
+
+
+
 // When the user scrolls the page, execute myFunction 
+var bill =0;
 window.onscroll = function() {myFunction()};
 function myFunction() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -8,10 +39,33 @@ function myFunction() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+
+
+//  ADD To Bill
 function ADDToBill (){
     bill+=10;
 }
 function PrintBill (){
     var x = document.getElementById("BillDiv");
-       x.innerHTML = "<h1>"+"Your Bill = " +bill +"$" +"</h1>" ;
+    
+       x.innerHTML = "<h1>"+"Have A Nice Day "+Name1+ "<br> Your Bill = " +bill +"$" +"</h1>" ;
+}
+
+
+
+
+// this is for image slide show 
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
