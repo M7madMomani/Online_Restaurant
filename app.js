9de771bf5875b74bc8c23cfe14027b91cdd2b0fb
MOHAMMAD ALMOMANI <MKHM.WORK@GMAIL.COM>
2021-01-30 22:16:58 +0200

    add new image

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var flag = true;
-while(flag  ==true){
+var flag2 = true;
+var flag3 = true;
+var image = '';
+var Order ="";
+var bill =0;
+var slideIndex = 0;
+
+
+
+while(flag==true){
     var Name1 =prompt('Whats your Name ').toUpperCase();
     ;
     if(Name1){
-        alert('Wlcome :- '+ Name1 )
         flag=false ;
     }
     else {
@@ -12,26 +20,39 @@ while(flag  ==true){
 }
 
 
+while(flag3==true){
+    var NumberOfPic =prompt('Whats your NumberOfPic');
+    if(NumberOfPic && NumberOfPic>0 ){
+        flag3=false ;
+    }
+    else {
+        alert('You should type your NumberOfPic' );
+    }
+}
 
-var flag2 = true;
-var Order ="";
-while(!(Order =="BURGER"|| Order =="PIZZA")){
+
+while(!(Order=="BURGER"|| Order=="PIZZA")){
     Order =prompt('WHAT\'S YOUR FAVORITE FOOD, BURGER OR PIZZA ?').toUpperCase();
 
     
     if(Order =="BURGER"){
         document.getElementById("burger").style.display = "block";
         
+        for(var i =0 ; i<NumberOfPic ; i++){
+            image =image +'<img src="https://i.pinimg.com/originals/63/0d/96/630d96bbb40088587c50e1fc7307c10a.png">';
+        }
     }
     else{
         document.getElementById("PIZZA").style.display = "block";
+        for(var i =0 ; i<NumberOfPic ; i++){
+            image =image +'<img src="https://seeklogo.com/images/P/pizza-logo-D075E874ED-seeklogo.com.png">';
+        }
     }
 }
 
 
-
 // When the user scrolls the page, execute myFunction 
-var bill =0;
+
 window.onscroll = function() {myFunction()};
 function myFunction() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -39,8 +60,6 @@ function myFunction() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
 }
-
-
 
 //  ADD To Bill
 function ADDToBill (){
@@ -52,11 +71,7 @@ function PrintBill (){
        x.innerHTML = "<h1>"+"Have A Nice Day "+Name1+ "<br> Your Bill = " +bill +"$" +"</h1>" ;
 }
 
-
-
-
 // this is for image slide show 
-var slideIndex = 0;
 showSlides();
 function showSlides() {
   var i;
@@ -69,3 +84,6 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+document.write(image)
